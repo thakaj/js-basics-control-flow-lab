7c5ad7429420c5d78ab92a31bd731ec913c87cfa
thakaj <taulant.hakaj@scranton.edu>
2021-05-18 14:32:28 -0400

    commit tree cc07ed772a46ceee3fbb4afc1977048d9a7cac2b

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+  return distance = 'This one is on me!';
+  }else if (distance >= 2001 && distance <= 2500){
+  return distance = `I will gladly take your thirty bucks.`
+  }else if (distance > 2500){
+    return distance = `No can do.`
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return (city === `NYC` ? `Ok, sounds good.`:`No go.`);{
+ } 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return `Thank you so much.`;
+      break;
+    case "not as generous":
+      return `Thank you.`;
+      break;
+    default:
+      return "Bye."
+      break;
+  }
 }
